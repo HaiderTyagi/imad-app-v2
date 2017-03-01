df60826e94001b00b54164b0c6a71fc4d65744b4
HaiderTyagi <haidertyagi@gmail.com>
2017-03-01 13:02:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,33 +18,9 @@ function createTemplate(data){
     var date=data.date;
     var content=data.content;
 
-var htmlTemplate='
-<html>
-    <head>
-    <title>
-    ${title}
-    </title>
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="center">
-           <a href="/">Home</a>
-        </div>
-        <br>
-         <h3>
-        ${heading}</h3>
-        <br>
-        <div>
-        ${date}</div>
-        <div class="center text-big bold">
-           ${content}
-        </div>
-        <script type="text/javascript" src="/ui/main.js">
-        </script>
-    </body>
-</html>';
+var htmlTemplate='<html>  <head>   <title> ${title}</title> <link href="/ui/style.css" rel="stylesheet" /></head><body><div class="center"><a href="/">Home</a></div> <br><h3> ${heading}</h3><br> <div>${date}</div> <div class="center text-big bold">  ${content}</div></body></html>';
 return htmltemplate;
-};
+}
 
 app.get('/article-one', function (req, res) {
   res.send(createTemplate(articleOne));
