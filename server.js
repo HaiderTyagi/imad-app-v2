9b827479d4b46d67a6506680ec495925cb917e02
HaiderTyagi <haidertyagi@gmail.com>
2017-03-04 11:52:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var app = express()
-app.use(morgan('combined'))
+var app = express();
+app.use(morgan('combined'));
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    });
+    
+    app.get('/ui/style.css', function(req, res){
+    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+    });
+    
+    app.get('/ui/madi.png', function(req, res){
+    res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+    });
+    
+    var port=8080;
+    app.listen(8080, function(){
+        
+        console.log('imad course app listening on port ${port}|');
+    });
