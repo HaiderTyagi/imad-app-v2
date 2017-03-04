fc4993ea24afb9575809ea749431f99334fca069
HaiderTyagi <haidertyagi@gmail.com>
2017-03-04 14:43:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
     });
     
-    app.get('/ui/main.js', function(req, res){
+   /* app.get('/ui/main.js', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-    });
+    });*/
     
     app.get('/ui/madi.png', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
